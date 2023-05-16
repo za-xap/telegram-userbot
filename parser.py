e13e21be0993ccd0ee65d56099489aa858c30d0d
Zakhar Prodan <ks7226599@gmail.com>
2023-05-16 09:16:29 +0000

    Fixed login error in parser
    Fixed KeyboardInterrupt in parser.py, because of conflict between sessions

--- parser.py
+++ parser.py
@@ -6,17 +6,18 @@ def main():
     LOGIN_URL = config.LOGIN_URL
     auth_data = {"LoginForm[username]":config.email, "LoginForm[password]":config.password}
     cookie = {"Cookie":"PHPSESSIDBACK=" + cookies.PHPSESSIDBACK}
-    r = requests.session().get(URL, headers=cookie)
+    with requests.session() as s:
+        r = s.get(URL, headers=cookie)
     if "Завдання" in r.text:
         pass
     elif "Завдання" not in r.text:
-        s = requests.session()
-        s.get(LOGIN_URL, data = auth_data)
-        r = s.get(URL)
-        c = s.cookies.get('PHPSESSIDBACK')
-        file = open('cookies.py', mode='w')
-        file.write("PHPSESSIDBACK = '" + c + "'")
-        file.close()
+        with requests.session() as s:
+            s.get(LOGIN_URL, data = auth_data)
+            r = s.get(URL)
+            c = s.cookies.get('PHPSESSIDBACK')
+            file = open('cookies.py', mode='w')
+            file.write("PHPSESSIDBACK = '" + c + "'")
+            file.close()
     else:
         pass
 
