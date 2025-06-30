7c69eaa0adb83cb2a93e1a38b1e3aca0bd4c0596
unknown <ks7226599@gmail.com>
2025-07-01 01:35:05 +0200

    Optimized user.py file, used PEP8 standart, deleted parser function, parser.py and cookies.py files

--- user.py
+++ user.py
@@ -1,55 +1,48 @@
 from telethon import functions, TelegramClient, events
-import datetime
-import json
 import arrow
 import asyncio
-import config, parser
-import signal
+import config
 import socket
 import urllib.request
-client = TelegramClient("telega", config.api_id, config.api_hash) #api_id and api_hash variables from config.py file
-@client.on(events.NewMessage(chats = "me", from_users = "me", pattern = "test")) #triggers on specific text in specific chat from specific user
-async def trigger(event): #answer with specific text
-    await event.reply("test") #or without reply #await client.send_message("me", "test")
-async def par():
-    signal.signal(signal.SIGINT, signal.SIG_DFL) #this restores the default Ctrl+C signal handler, which just kills the process
-    a = 0
-    while True:
-        b = parser.main()
-        if a == 0 and b == 0:
-            pass
-        elif a == 0 and b == 1:
-            a = 1
-            await client.send_message("me", "New action!", schedule = datetime.datetime.now() + datetime.timedelta(seconds=10))
-        elif a == 1 and b == 1:
-            pass
-        elif a == 1 and b == 0:
-            a = 0
-        await asyncio.sleep(20)
-async def main(): #updating bio to text + local time with nice font
+
+client = TelegramClient("telega", config.api_id, config.api_hash)  # api_id and api_hash variables from config.py file
+
+
+@client.on(events.NewMessage(chats="me", from_users="me", pattern="test"))  # triggers on pattern text in chat from user
+async def trigger(event):  # answer with specific text
+    await event.reply("test")  # or without reply #await client.send_message("me", "test")
+
+
+async def main():  # updating bio to text + local time with nice font
     prew_date = "0"
+    digits = {
+        '0': chr(0x1D7EC), '1': chr(0x1D7ED), '2': chr(0x1D7EE),
+        '3': chr(0x1D7EF), '4': chr(0x1D7F0), '5': chr(0x1D7F1),
+        '6': chr(0x1D7F2), '7': chr(0x1D7F3), '8': chr(0x1D7F4),
+        '9': chr(0x1D7F5),
+    }
     while True:
         utc = arrow.utcnow()
-        local = utc.to("Europe/Warsaw") #Kiev or Warsaw
+        local = utc.to("Europe/Warsaw")  # Kiev or Warsaw
         local_date = local.format('H:mm')
-        local_date = local_date.translate(local_date.maketrans("0123456789","ghijklmnop"))
-        r = [['g', '\\uD835\\uDFEC'], ['h', '\\uD835\\uDFED'], ['i', '\\uD835\\uDFEE'], ['j', '\\uD835\\uDFEF'], ['k', '\\uD835\\uDFF0'], ['l', '\\uD835\\uDFF1'], ['m', '\\uD835\\uDFF2'], ['n', '\\uD835\\uDFF3'], ['o', '\\uD835\\uDFF4'], ['p', '\\uD835\\uDFF5']]
-        for e in r:
-            local_date=local_date.replace(e[0],e[1])
+        local_date = ''.join(digits.get(c, c) for c in local_date)
         if local_date != prew_date:
-            await client(functions.account.UpdateProfileRequest(about="The risk was calculated, but I'm bad at math. My time - " + json.loads('\"'+local_date+'\"')))
+            await client(functions.account.UpdateProfileRequest(
+                about=f"The risk was calculated, but I'm bad at math. My time - {local_date}"))
             prew_date = local_date
         await asyncio.sleep(1)
+
+
 async def downdetector():
     while True:
         try:
-            urllib.request.urlopen("https://hc-ping.com/" + config.hc_id, timeout=10)
+            urllib.request.urlopen("https://hc-ping.com/" + config.hc_id, timeout=10)  # hc_id variable from config.py
         except socket.error as e:
-            print("Ping failed: %s" % e)
+            print(f"Ping failed: {e}")
         await asyncio.sleep(30)
+
+
 with client:
-    loop = asyncio.get_event_loop()
-    client.loop.create_task(par())
     client.loop.create_task(main())
     client.loop.create_task(downdetector())
-    client.loop.run_forever()
+    client.run_until_disconnected()
